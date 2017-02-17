f5c7266b080e6afac03b51e14f77cdf6f3c23fcd
Bryan <bryan.lloyd@hatterasfunds.com>
2017-02-17 14:32:35 -0500

    deleted extraneuous, experimental scripts changed cull_ggplot_data

--- R/cull_ggplot_data.R
+++ R/cull_ggplot_data.R
@@ -1,14 +1,29 @@
-cull_ggplot_data = function(xts_data, cols = names(xts_data), cumulativeCols = NULL, ...){
+cull_ggplot_data = function(xts_data, melt_cols = names(xts_data), cumulativeCols = NULL, ...){
   # if(!is.null(na.action))
   #   xts_data = na.action(xts_data)
+  if(!lubridate::is.Date(index(xts_data)))
+    index(xts_data) = zoo::as.Date(index(xts_data))
+
+  freq = PerformanceAnalytics::Frequency(xts_data)[1]
+  if(freq == 12){
+    per = "months"
+  } else
+  if (freq == 4) {
+    per = "quarters"
+  } else {
+    per = "years"
+  }
+
+  index(xts_data) = lubridate::ceiling_date(index(xts_data), unit = per) - 1
+
 
   if(!is.null(cumulativeCols))
     xts_data[,cumulativeCols] = cumsum(na.fill(xts_data[, cumulativeCols], 0))
 
 
-  df = data.frame(date = zoo::index(xts_data), xts_data[, cols])
+  df = data.frame(date = zoo::index(xts_data), xts_data)
   df_melt = data.table::melt(data = df,
-                             id.vars = c("date"),
-                             measure.vars = cols, ...)
+                             id.vars = names(df)[which(!(names(df) %in% melt_cols))],
+                             measure.vars = melt_cols)
   return(df_melt)
 }
